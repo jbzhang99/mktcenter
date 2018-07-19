28a1f4e92afdb9ea52738045fd2e3c143b425494
agan <15853150670@168.com>
2018-07-19 10:25:09 +0800

    新增job工具类

--- mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/service/impl/ActivityRegisterServiceImpl.java
+++ mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/service/impl/ActivityRegisterServiceImpl.java
@@ -5,27 +5,22 @@ import com.bizvane.mktcenterservice.models.bo.ActivityBO;
 import com.bizvane.mktcenterservice.models.po.*;
 import com.bizvane.mktcenterservice.models.vo.ActivityVO;
 import com.bizvane.mktcenterservice.models.vo.MessageVO;
-import com.bizvane.mktcenterserviceimpl.common.constants.ActivityConstants;
-import com.bizvane.mktcenterserviceimpl.common.constants.JobHandlerConstants;
 import com.bizvane.mktcenterserviceimpl.common.enums.ActivityStatusEnum;
 import com.bizvane.mktcenterserviceimpl.common.enums.BusinessTypeEnum;
 import com.bizvane.mktcenterserviceimpl.common.job.XxlJobConfig;
-import com.bizvane.mktcenterserviceimpl.common.utils.DateUtil;
+import com.bizvane.mktcenterserviceimpl.common.utils.JobUtil;
 import com.bizvane.mktcenterserviceimpl.mappers.MktActivityPOMapper;
 import com.bizvane.mktcenterserviceimpl.mappers.MktActivityRegisterPOMapper;
 import com.bizvane.mktcenterserviceimpl.mappers.MktCouponPOMapper;
 import com.bizvane.mktcenterserviceimpl.mappers.MktMessagePOMapper;
 import com.bizvane.utils.commonutils.PageForm;
-import com.bizvane.utils.enumutils.JobEnum;
 import com.bizvane.utils.jobutils.JobClient;
-import com.bizvane.utils.jobutils.XxlJobInfo;
 import com.bizvane.utils.responseinfo.ResponseData;
 import com.bizvane.utils.tokens.SysAccountPO;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
@@ -58,7 +53,8 @@ public class ActivityRegisterServiceImpl implements ActivityRegisterService {
 
     @Autowired
     private JobClient jobClient;
-
+    @Autowired
+    private JobUtil jobUtil;
     /**
      * 查询活动列表
      * @param vo
@@ -112,32 +108,7 @@ public class ActivityRegisterServiceImpl implements ActivityRegisterService {
 
         //根据上面的状态添加调度，参数是活动id
         if(ActivityStatusEnum.ACTIVITY_STATUS_PENDING.getCode() == mktActivityPOWithBLOBs.getActivityStatus()){
-            //活动状态设置为待执行
-            mktActivityPOWithBLOBs.setActivityStatus(ActivityStatusEnum.ACTIVITY_STATUS_PENDING.getCode());
-            //构建job对象
-            XxlJobInfo xxlJobInfo = new XxlJobInfo();
-            //设置appName
-            xxlJobInfo.setAppName(xxlJobConfig.getAppName());
-            //设置路由策略
-            xxlJobInfo.setExecutorRouteStrategy(JobEnum.EXECUTOR_ROUTE_STRATEGY_FIRST.getValue());
-            //设置job定时器
-            xxlJobInfo.setJobCron(DateUtil.getCronExpression(activityVO.getStartTime()));
-            //设置运行模式
-            xxlJobInfo.setGlueType(JobEnum.GLUE_TYPE_BEAN.getValue());
-            //设置job处理器
-            xxlJobInfo.setExecutorHandler(JobHandlerConstants.activity);
-            //设置job描述
-            xxlJobInfo.setJobDesc(activityVO.getActivityInfo());
-            //设置执行参数
-            xxlJobInfo.setExecutorParam(mktActivityId.toString());
-            //设置阻塞处理策略
-            xxlJobInfo.setExecutorBlockStrategy(JobEnum.EXECUTOR_BLOCK_SERIAL_EXECUTION.getValue());
-            //设置失败处理策略
-            xxlJobInfo.setExecutorFailStrategy(JobEnum.EXECUTOR_FAIL_STRATEGY_NULL.getValue());
-            //设置负责人
-            xxlJobInfo.setAuthor(stageUser.getName());
-            //添加job
-            jobClient.addJob(xxlJobInfo);
+            jobUtil.addJob(stageUser,activityVO,mktActivityPOWithBLOBs,mktActivityId);
         }
 
         //新增开卡活动表
@@ -175,6 +146,7 @@ public class ActivityRegisterServiceImpl implements ActivityRegisterService {
         //结束
         return responseData;
     }
+
 
     /**
      * 执行活动
