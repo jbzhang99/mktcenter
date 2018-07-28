7b4797561aba4b3bdfb4ae3b0bdd751da1ee9d5b
gengxiaoyu <wohen12336>
2018-07-28 10:50:50 +0800

    0

--- mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/service/impl/TaskProfileServiceImpl.java
+++ mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/service/impl/TaskProfileServiceImpl.java
@@ -1,19 +1,27 @@
 package com.bizvane.mktcenterserviceimpl.service.impl;
 
+import com.bizvane.centerstageservice.models.po.SysCheckConfigPo;
+import com.bizvane.centerstageservice.models.vo.SysCheckConfigVo;
+import com.bizvane.centerstageservice.rpc.SysCheckConfigServiceRpc;
 import com.bizvane.mktcenterservice.interfaces.TaskProfileService;
 import com.bizvane.mktcenterservice.models.bo.TaskBO;
 import com.bizvane.mktcenterservice.models.vo.ActivityVO;
 import com.bizvane.mktcenterservice.models.vo.TaskVO;
+import com.bizvane.mktcenterserviceimpl.common.enums.TaskTypeEnum;
 import com.bizvane.mktcenterserviceimpl.mappers.MktTaskProfilePOMapper;
 import com.bizvane.utils.commonutils.PageForm;
+import com.bizvane.utils.enumutils.SysResponseEnum;
 import com.bizvane.utils.responseinfo.ResponseData;
 import com.bizvane.utils.tokens.SysAccountPO;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @author chen.li
@@ -26,6 +34,9 @@ public class TaskProfileServiceImpl implements TaskProfileService {
 
     @Autowired
     private MktTaskProfilePOMapper mktTaskProfilePOMapper;
+
+    @Autowired
+    private SysCheckConfigServiceRpc sysCheckConfigServiceRpc;
 
     /**
      * 获取任务列表
@@ -51,7 +62,59 @@ public class TaskProfileServiceImpl implements TaskProfileService {
      */
     @Override
     public ResponseData<Integer> addTask(TaskBO bo, SysAccountPO stageUser) {
+        //创建任务对象
         TaskVO taskVO = bo.getTaskVO();
+        //需要返回的对象
+        ResponseData responseData = new ResponseData();
+        //UUID生成任务编号 实际生成规则？？？？
+        String taskCode = "TC"+UUID.randomUUID().toString().replaceAll("-","");
+        //将任务编号添加进vo对象
+        taskVO.setTaskCode(taskCode);
+        //添加任务类型为完善资料
+        taskVO.setTaskType(TaskTypeEnum.TASK_TYPE_PROFILE.getCode());
+
+
+
+        //判断完善资料任务是否有重复
+        TaskVO vo =  new TaskVO();
+        //完善资料是针对品牌还是商家？conpany or brand？and此处的id到底是stageuser携带过来额还是taskvo携带来的
+        vo.setSysCompanyId(stageUser.getSysCompanyId());
+        vo.setTaskType(taskVO.getTaskType());//或者直接填完善资料的任务类型???
+        List<TaskVO> taskVOList = mktTaskProfilePOMapper.getTaskList(vo);
+        //集合不为空，则说明设置重复
+        if (!CollectionUtils.isEmpty(taskVOList)){
+            responseData.setCode(SysResponseEnum.FAILED.getCode());
+            responseData.setMessage("完善资料任务已存在");
+            return responseData;
+        }
+
+
+        //判断任务是否需要审核(未完成)
+
+        SysCheckConfigVo sysCheckConfigVo = new SysCheckConfigVo();
+        sysCheckConfigVo.setSysBrandId(taskVO.getSysBrandId());
+        sysCheckConfigVo.setFunctionCode(taskVO.);
+        ResponseData<List<SysCheckConfigVo>> rls = sysCheckConfigServiceRpc.getCheckConfigListAll(sysCheckConfigVo);
+        List<SysCheckConfigVo> sysCheckConfigVoList = rls.getData();
+        int i = 0;
+
+        if(!CollectionUtils.isEmpty(sysCheckConfigVoList)){
+            for(SysCheckConfigVo sysCheckConfigVo1:sysCheckConfigVoList){
+
+
+
+            }
+
+        }
+        //如果需要审核
+
+
+
+
+
+
+
+
 
         return null;
     }
