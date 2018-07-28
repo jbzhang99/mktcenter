588ab8abbdaf0020125f72368214f5ea8e6fcd2e
ljw <junweili1992@163.com>
2018-07-28 15:53:17 +0800

    code生成

---
+++ mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/common/utils/CodeUtil.java
@@ -0,0 +1,38 @@
+package com.bizvane.mktcenterserviceimpl.common.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @Author: lijunwei
+ * @Time: 2018/7/28 15:52
+ */
+public class CodeUtil {
+    private  final  static String  TASK_PREFIX="T";
+    private  final  static String  ACTIVITY_PREFIX="A";
+    private  final  static SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+
+    public static String  getTaskCode(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(TASK_PREFIX);
+        getRaCodedom(stringBuilder);
+        return stringBuilder.toString();
+    }
+
+    public static String  getActivityCode(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(ACTIVITY_PREFIX);
+        getRaCodedom(stringBuilder);
+        return stringBuilder.toString();
+    }
+
+    private static StringBuilder getRaCodedom(StringBuilder stringBuilder) {
+        stringBuilder.append(sdf.format(new Date()));
+        stringBuilder.append(Math.round(Math.random() * 9));
+        return stringBuilder;
+    }
+}
+
+
+
+
