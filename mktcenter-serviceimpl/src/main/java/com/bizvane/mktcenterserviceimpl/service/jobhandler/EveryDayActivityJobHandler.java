b682f2b6af72301dba1df01acb24016083e3de48
agan <15853150670@168.com>
2018-08-01 16:59:38 +0800

    新增job

--- mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/service/jobhandler/EveryDayActivityJobHandler.java
+++ mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/service/jobhandler/EveryDayActivityJobHandler.java
@@ -1,7 +1,7 @@
 package com.bizvane.mktcenterserviceimpl.service.jobhandler;
 
 import com.bizvane.mktcenterservice.models.po.MktActivityPO;
-import com.bizvane.mktcenterserviceimpl.common.enums.ActivityStatusEnum;
+import com.bizvane.mktcenterservice.models.po.MktActivityPOExample;
 import com.bizvane.mktcenterserviceimpl.mappers.MktActivityPOMapper;
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.IJobHandler;
@@ -9,26 +9,28 @@ import com.xxl.job.core.handler.annotation.JobHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 /**
- * Created by agan on 2018/7/27.
+ * Created by agan on 2018/8/1.
  */
-@JobHandler(value="endActivity")
+@JobHandler(value="everyDayActivityBirthday")
 @Component
-public class ActivityJobEndHandler extends IJobHandler {
+public class EveryDayActivityJobHandler extends IJobHandler {
     @Autowired
     private MktActivityPOMapper mktActivityPOMapper;
     @Override
     public ReturnT<String> execute(String param) throws Exception {
         System.out.println("job执行参数 "+param);
         ReturnT returnT = new ReturnT();
-        MktActivityPO po = new MktActivityPO();
-        po.setActivityCode(param);
-        po.setActivityStatus(ActivityStatusEnum.ACTIVITY_STATUS_FINISHED.getCode());
-        //把活动状态改成已结束
-        int sum = mktActivityPOMapper.updateActivityStatus(po);
-        returnT.setCode(0);
-        returnT.setContent("活动执行完毕");
-        returnT.setMsg("success");
-        return returnT;
+        System.out.println("开始执行活动");
+        //查询该活动是否已经审核
+        MktActivityPOExample examplem = new MktActivityPOExample();
+        examplem.createCriteria().andActivityCodeEqualTo(param);
+        examplem.createCriteria().andValidEqualTo(true);
+        List<MktActivityPO> mktActivityPOs = mktActivityPOMapper.selectByExample(examplem);
+
+        //查询品牌下所有会员 发送奖励TODO
+        return null;
     }
 }
