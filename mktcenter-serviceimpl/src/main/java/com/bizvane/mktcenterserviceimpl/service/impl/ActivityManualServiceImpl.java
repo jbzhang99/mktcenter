fbb389fc721ffebc6883f36579ce0a47c0254366
shuxiao <shuxiao@bizvane.cn>
2018-08-01 20:24:13 +0800

    领券活动

--- mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/service/impl/ActivityManualServiceImpl.java
+++ mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/service/impl/ActivityManualServiceImpl.java
@@ -145,9 +145,9 @@ public class ActivityManualServiceImpl implements ActivityManualService {
             //getStartTime 开始时间>当前时间增加job
             if (new Date().before(activityVO.getStartTime())) {
                 //创建任务调度任务开始时间
-                jobUtil.addJob(stageUser, activityVO, mktActivityPOWithBLOBs, activityCode);
+                jobUtil.addJob(stageUser, activityVO, activityCode);
                 //创建任务调度任务结束时间
-                jobUtil.addJobEndTime(stageUser, activityVO, mktActivityPOWithBLOBs, activityCode);
+                jobUtil.addJobEndTime(stageUser, activityVO, activityCode);
             }
         } else {
             //查询结果如果不需要审核审核状态为已审核
@@ -157,9 +157,9 @@ public class ActivityManualServiceImpl implements ActivityManualService {
                 //活动状态设置为待执行
                 mktActivityPOWithBLOBs.setActivityStatus(ActivityStatusEnum.ACTIVITY_STATUS_PENDING.getCode());
                 //创建任务调度任务开始时间
-                jobUtil.addJob(stageUser, activityVO, mktActivityPOWithBLOBs, activityCode);
+                jobUtil.addJob(stageUser, activityVO, activityCode);
                 //创建任务调度任务结束时间
-                jobUtil.addJobEndTime(stageUser, activityVO, mktActivityPOWithBLOBs, activityCode);
+                jobUtil.addJobEndTime(stageUser, activityVO, activityCode);
             } else {
                 //活动状态设置为执行中
                 mktActivityPOWithBLOBs.setActivityStatus(ActivityStatusEnum.ACTIVITY_STATUS_EXECUTING.getCode());
