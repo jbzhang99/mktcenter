050d675924e12fac3d39edae6a27b76273cc4fb1
chen.li <lich1n@163.com>
2018-08-01 09:22:02 +0800

    记录表增加积分

--- mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/mappers/MktActivityRecordPOMapper.java
+++ mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/mappers/MktActivityRecordPOMapper.java
@@ -9,69 +9,69 @@ public interface MktActivityRecordPOMapper {
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-07-31 19:48:47
+	 * @mbg.generated  2018-08-01 09:21:14
 	 */
 	long countByExample(MktActivityRecordPOExample example);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-07-31 19:48:47
+	 * @mbg.generated  2018-08-01 09:21:14
 	 */
 	int deleteByExample(MktActivityRecordPOExample example);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-07-31 19:48:47
+	 * @mbg.generated  2018-08-01 09:21:14
 	 */
 	int deleteByPrimaryKey(Long mktActivityRecordId);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-07-31 19:48:47
+	 * @mbg.generated  2018-08-01 09:21:14
 	 */
 	int insert(MktActivityRecordPO record);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-07-31 19:48:47
+	 * @mbg.generated  2018-08-01 09:21:14
 	 */
 	int insertSelective(MktActivityRecordPO record);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-07-31 19:48:47
+	 * @mbg.generated  2018-08-01 09:21:14
 	 */
 	List<MktActivityRecordPO> selectByExample(MktActivityRecordPOExample example);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-07-31 19:48:47
+	 * @mbg.generated  2018-08-01 09:21:14
 	 */
 	MktActivityRecordPO selectByPrimaryKey(Long mktActivityRecordId);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-07-31 19:48:47
+	 * @mbg.generated  2018-08-01 09:21:14
 	 */
 	int updateByExampleSelective(@Param("record") MktActivityRecordPO record,
 			@Param("example") MktActivityRecordPOExample example);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-07-31 19:48:47
+	 * @mbg.generated  2018-08-01 09:21:14
 	 */
 	int updateByExample(@Param("record") MktActivityRecordPO record,
 			@Param("example") MktActivityRecordPOExample example);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-07-31 19:48:47
+	 * @mbg.generated  2018-08-01 09:21:14
 	 */
 	int updateByPrimaryKeySelective(MktActivityRecordPO record);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-07-31 19:48:47
+	 * @mbg.generated  2018-08-01 09:21:14
 	 */
 	int updateByPrimaryKey(MktActivityRecordPO record);
 }
