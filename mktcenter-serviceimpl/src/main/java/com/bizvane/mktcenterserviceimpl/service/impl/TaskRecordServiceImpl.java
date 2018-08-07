05439146b5d77a5026bc6151b605ef6672b7989f
ljw <junweili1992@163.com>
2018-08-07 10:40:06 +0800

    记录数据

--- mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/service/impl/TaskRecordServiceImpl.java
+++ mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/service/impl/TaskRecordServiceImpl.java
@@ -1,6 +1,11 @@
 package com.bizvane.mktcenterserviceimpl.service.impl;
 
 import com.bizvane.mktcenterservice.interfaces.TaskRecordService;
+import com.bizvane.mktcenterservice.models.po.MktTaskRecordPO;
+import com.bizvane.mktcenterserviceimpl.common.utils.TimeUtils;
+import com.bizvane.mktcenterserviceimpl.mappers.MktTaskRecordPOMapper;
+import com.bizvane.utils.tokens.SysAccountPO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,4 +14,32 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class TaskRecordServiceImpl implements TaskRecordService {
+
+    @Autowired
+    private MktTaskRecordPOMapper mktTaskRecordPOMapper;
+
+    /**
+     * 添加记录
+     * @param po
+     * @param stageUser
+     * @return
+     */
+    @Override
+    public Integer addTaskRecord(MktTaskRecordPO po, SysAccountPO stageUser) {
+        po.setCreateDate(TimeUtils.getNowTime());
+        po.setCreateUserId(stageUser.getSysAccountId());
+        po.setCreateUserName(stageUser.getName());
+        return mktTaskRecordPOMapper.insertSelective(po);
+
+    }
+    /**
+     * 修改记录
+     */
+    @Override
+    public Integer updateTaskRecord(MktTaskRecordPO po, SysAccountPO stageUser) {
+        po.setModifiedDate(TimeUtils.getNowTime());
+        po.setModifiedUserId(stageUser.getSysAccountId());
+        po.setModifiedUserName(stageUser.getName());
+        return mktTaskRecordPOMapper.insertSelective(po);
+    }
 }
