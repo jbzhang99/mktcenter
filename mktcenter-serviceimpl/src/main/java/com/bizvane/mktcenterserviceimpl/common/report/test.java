5eb3549ac32f70a8a559dbe244cf1f40eef2cde4
lijunwei <lijunwei@bizvane.cn>
2018-10-13 19:18:08 +0800

    订单类任务的修改ok002

--- mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/common/report/test.java
+++ mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/common/report/test.java
@@ -8,17 +8,17 @@ import com.bizvane.mktcenterservice.models.vo.TaskVO;
 import com.bizvane.utils.jobutils.JobClient;
 
 public class test {
-	
+
     @Autowired
     private static JobClient jobClient;
     
 	public static void seng() throws Exception {
 
-//		
+//
 //	    ResponseEntity<String> stringResponseEntity = jobClient.addJob();
 //	    TaskVO smsSingleResponse2 = JSON.parseObject("", TaskVO.class);
 //	    System.out.println("response  toString is :" + smsSingleResponse2);
-       
+
 	}
     
 	public static void main(String[] args) throws Exception {
