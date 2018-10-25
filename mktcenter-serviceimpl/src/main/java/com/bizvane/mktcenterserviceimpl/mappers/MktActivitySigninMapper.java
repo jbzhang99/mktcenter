e829d5d2e624847eb7b8f348537bcadd9c7547e6
chen.li <lich1n@163.com>
2018-10-25 14:39:41 +0800

    签到门店

--- mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/mappers/MktActivitySigninMapper.java
+++ mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/mappers/MktActivitySigninMapper.java
@@ -11,68 +11,87 @@ public interface MktActivitySigninMapper {
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-25 14:39:03
 	 */
 	long countByExample(MktActivitySigninExample example);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-25 14:39:03
 	 */
 	int deleteByExample(MktActivitySigninExample example);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-25 14:39:03
 	 */
 	int deleteByPrimaryKey(Long mktActivitySignInId);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-25 14:39:03
 	 */
 	int insert(MktActivitySignin record);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-25 14:39:03
 	 */
 	int insertSelective(MktActivitySignin record);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-25 14:39:03
+	 */
+	List<MktActivitySignin> selectByExampleWithBLOBs(MktActivitySigninExample example);
+
+	/**
+	 * 只读. 
+	 * @mbg.generated  2018-10-25 14:39:03
 	 */
 	List<MktActivitySignin> selectByExample(MktActivitySigninExample example);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-25 14:39:03
 	 */
 	MktActivitySignin selectByPrimaryKey(Long mktActivitySignInId);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-25 14:39:03
 	 */
 	int updateByExampleSelective(@Param("record") MktActivitySignin record,
 			@Param("example") MktActivitySigninExample example);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-25 14:39:03
+	 */
+	int updateByExampleWithBLOBs(@Param("record") MktActivitySignin record,
+			@Param("example") MktActivitySigninExample example);
+
+	/**
+	 * 只读. 
+	 * @mbg.generated  2018-10-25 14:39:03
 	 */
 	int updateByExample(@Param("record") MktActivitySignin record, @Param("example") MktActivitySigninExample example);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-25 14:39:03
 	 */
 	int updateByPrimaryKeySelective(MktActivitySignin record);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-25 14:39:03
+	 */
+	int updateByPrimaryKeyWithBLOBs(MktActivitySignin record);
+
+	/**
+	 * 只读. 
+	 * @mbg.generated  2018-10-25 14:39:03
 	 */
 	int updateByPrimaryKey(MktActivitySignin record);
 
