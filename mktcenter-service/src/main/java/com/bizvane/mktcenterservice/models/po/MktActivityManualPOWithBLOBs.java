ecd92237596c75a6d2810e9c3842ca44a2a2709b
chen.li <lich1n@163.com>
2018-10-31 10:05:37 +0800

    领券门店

---
+++ mktcenter-service/src/main/java/com/bizvane/mktcenterservice/models/po/MktActivityManualPOWithBLOBs.java
@@ -0,0 +1,66 @@
+package com.bizvane.mktcenterservice.models.po;
+
+import java.io.Serializable;
+
+public class MktActivityManualPOWithBLOBs extends MktActivityManualPO implements Serializable {
+    /**
+     * 只读.
+    二维码地址. qrcode
+     *
+     * @mbg.generated 2018-10-31 10:05:06
+     */
+    @io.swagger.annotations.ApiModelProperty(value = "二维码地址",name = "qrcode", required = false,example = "")
+    private String qrcode;
+
+    /**
+     * 只读.
+    门店限制名单. store_limit_list
+     *
+     * @mbg.generated 2018-10-31 10:05:06
+     */
+    @io.swagger.annotations.ApiModelProperty(value = "门店限制名单",name = "storeLimitList", required = false,example = "")
+    private String storeLimitList;
+
+    /**
+     * 只读. 
+     *
+     * @mbg.generated 2018-10-31 10:05:06
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 只读. 
+     *
+     * @mbg.generated 2018-10-31 10:05:06
+     */
+    public String getQrcode() {
+        return qrcode;
+    }
+
+    /**
+     * 只读. 
+     *
+     * @mbg.generated 2018-10-31 10:05:06
+     */
+    public void setQrcode(String qrcode) {
+        this.qrcode = qrcode == null ? null : qrcode.trim();
+    }
+
+    /**
+     * 只读. 
+     *
+     * @mbg.generated 2018-10-31 10:05:06
+     */
+    public String getStoreLimitList() {
+        return storeLimitList;
+    }
+
+    /**
+     * 只读. 
+     *
+     * @mbg.generated 2018-10-31 10:05:06
+     */
+    public void setStoreLimitList(String storeLimitList) {
+        this.storeLimitList = storeLimitList == null ? null : storeLimitList.trim();
+    }
+}
--- mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/mappers/MktActivityManualPOMapper.java
+++ mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/mappers/MktActivityManualPOMapper.java
@@ -7,92 +7,93 @@ import com.bizvane.mktcenterservice.models.vo.ActivityVO;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import com.bizvane.mktcenterservice.models.po.MktActivityManualPOWithBLOBs;
 
 public interface MktActivityManualPOMapper {
     /**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-31 10:05:06
 	 */
 	long countByExample(MktActivityManualPOExample example);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-31 10:05:06
 	 */
 	int deleteByExample(MktActivityManualPOExample example);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-31 10:05:06
 	 */
 	int deleteByPrimaryKey(Long mktActivityManualId);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-31 10:05:06
 	 */
-	int insert(MktActivityManualPO record);
+	int insert(MktActivityManualPOWithBLOBs record);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-31 10:05:06
 	 */
-	int insertSelective(MktActivityManualPO record);
+	int insertSelective(MktActivityManualPOWithBLOBs record);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-31 10:05:06
 	 */
-	List<MktActivityManualPO> selectByExampleWithBLOBs(MktActivityManualPOExample example);
+	List<MktActivityManualPOWithBLOBs> selectByExampleWithBLOBs(MktActivityManualPOExample example);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-31 10:05:06
 	 */
 	List<MktActivityManualPO> selectByExample(MktActivityManualPOExample example);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-31 10:05:06
 	 */
-	MktActivityManualPO selectByPrimaryKey(Long mktActivityManualId);
+	MktActivityManualPOWithBLOBs selectByPrimaryKey(Long mktActivityManualId);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-31 10:05:06
 	 */
-	int updateByExampleSelective(@Param("record") MktActivityManualPO record,
+	int updateByExampleSelective(@Param("record") MktActivityManualPOWithBLOBs record,
 			@Param("example") MktActivityManualPOExample example);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-31 10:05:06
 	 */
-	int updateByExampleWithBLOBs(@Param("record") MktActivityManualPO record,
+	int updateByExampleWithBLOBs(@Param("record") MktActivityManualPOWithBLOBs record,
 			@Param("example") MktActivityManualPOExample example);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-31 10:05:06
 	 */
 	int updateByExample(@Param("record") MktActivityManualPO record,
 			@Param("example") MktActivityManualPOExample example);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-31 10:05:06
 	 */
-	int updateByPrimaryKeySelective(MktActivityManualPO record);
+	int updateByPrimaryKeySelective(MktActivityManualPOWithBLOBs record);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-31 10:05:06
 	 */
-	int updateByPrimaryKeyWithBLOBs(MktActivityManualPO record);
+	int updateByPrimaryKeyWithBLOBs(MktActivityManualPOWithBLOBs record);
 
 	/**
 	 * 只读. 
-	 * @mbg.generated  2018-08-31 12:40:26
+	 * @mbg.generated  2018-10-31 10:05:06
 	 */
 	int updateByPrimaryKey(MktActivityManualPO record);
 
