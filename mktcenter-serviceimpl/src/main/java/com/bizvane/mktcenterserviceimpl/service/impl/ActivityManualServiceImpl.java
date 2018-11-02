96675d99604a9c27e6dac91c333c7d62b64a5d82
agan <15853150670@168.com>
2018-11-02 17:16:29 +0800

    修改智能营销

--- mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/service/impl/ActivityManualServiceImpl.java
+++ mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/service/impl/ActivityManualServiceImpl.java
@@ -148,9 +148,9 @@ public class ActivityManualServiceImpl implements ActivityManualService {
 
 
         //查询审核配置，是否需要审核然后判断
-        SysCheckConfigVo so = new SysCheckConfigVo();
-        so.setSysBrandId(activityVO.getSysBrandId());
-        ResponseData<List<SysCheckConfigVo>> sysCheckConfigVo = sysCheckConfigServiceRpc.getCheckConfigListAll(so);
+       /* SysCheckConfigVo so = new SysCheckConfigVo();
+        so.setSysBrandId(activityVO.getSysBrandId());*/
+        ResponseData<List<SysCheckConfigVo>> sysCheckConfigVo = sysCheckConfigServiceRpc.getCheckConfigListAll(activityVO.getSysBrandId());
         List<SysCheckConfigVo> sysCheckConfigVoList = sysCheckConfigVo.getData();
         //判断是否有审核配置
         int i = 0;
--- mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/service/impl/ActivityUpgradeServiceImpl.java
+++ mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/service/impl/ActivityUpgradeServiceImpl.java
@@ -181,9 +181,9 @@ public class ActivityUpgradeServiceImpl implements ActivityUpgradeService {
 
 
         //查询审核配置，是否需要审核然后判断
-        SysCheckConfigVo so = new SysCheckConfigVo();
-        so.setSysBrandId(activityVO.getSysBrandId());
-        ResponseData<List<SysCheckConfigVo>> sysCheckConfigVo =sysCheckConfigServiceRpc.getCheckConfigListAll(so);
+        /*SysCheckConfigVo so = new SysCheckConfigVo();
+        so.setSysBrandId(activityVO.getSysBrandId());*/
+        ResponseData<List<SysCheckConfigVo>> sysCheckConfigVo =sysCheckConfigServiceRpc.getCheckConfigListAll(activityVO.getSysBrandId());
         List<SysCheckConfigVo> sysCheckConfigVoList = sysCheckConfigVo.getData();
         //判断是否有审核配置
         int i = 0;
@@ -357,9 +357,9 @@ public class ActivityUpgradeServiceImpl implements ActivityUpgradeService {
         xxlJobInfo.setExecutorParam(activityVO.getActivityCode());
         xxlJobInfo.setBizType(BusinessTypeEnum.ACTIVITY_TYPE_ACTIVITY.getCode());
         //查询审核配置，是否需要审核然后判断
-        SysCheckConfigVo so = new SysCheckConfigVo();
-        so.setSysBrandId(activityVO.getSysBrandId());
-        ResponseData<List<SysCheckConfigVo>> sysCheckConfigVo =sysCheckConfigServiceRpc.getCheckConfigListAll(so);
+       /* SysCheckConfigVo so = new SysCheckConfigVo();
+        so.setSysBrandId(activityVO.getSysBrandId());*/
+        ResponseData<List<SysCheckConfigVo>> sysCheckConfigVo =sysCheckConfigServiceRpc.getCheckConfigListAll(activityVO.getSysBrandId());
         List<SysCheckConfigVo> sysCheckConfigVoList = sysCheckConfigVo.getData();
         //判断是否有审核配置
         int i = 0;
