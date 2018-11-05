d207fd4404b98eb92dfc993172fbcf90cde761aa
agan <15853150670@168.com>
2018-11-05 17:23:44 +0800

    修改活动

--- mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/service/impl/ActivityManualServiceImpl.java
+++ mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/service/impl/ActivityManualServiceImpl.java
@@ -133,7 +133,7 @@ public class ActivityManualServiceImpl implements ActivityManualService {
             return responseData;
         }
         //判断活动开始时间是否大于当前时间
-        if(1 != bo.getActivityVO().getLongTerm() && new Date().after(activityVO.getStartTime())){
+        if(new Date().after(activityVO.getStartTime())){
             responseData.setCode(SysResponseEnum.MODEL_FAILED_VALIDATION.getCode());
             responseData.setMessage("活动开始时间不能比当前时间小!");
             return responseData;
