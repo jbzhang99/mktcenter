bba4bceb371dd65e3a0b42150b219a4bf07dba23
agan <15853150670@168.com>
2018-12-19 17:26:19 +0800

    修改问题

---
+++ mktcenter-service/src/main/java/com/bizvane/mktcenterservice/models/vo/MktActivityPrizeRecordVO.java
@@ -0,0 +1,9 @@
+package com.bizvane.mktcenterservice.models.vo;
+
+import com.bizvane.mktcenterservice.models.po.MktActivityPrizeRecordPO;
+
+/**
+ * Created by agan on 2018/12/19.
+ */
+public class MktActivityPrizeRecordVO extends MktActivityPrizeRecordPO {
+}
