8e3db93ea0b6f5a336a8bbbe8335d26c0173c346
lijunwei <lijunwei@bizvane.cn>
2018-12-19 17:31:15 +0800

    大转盘

--- mktcenter-service/src/main/java/com/bizvane/mktcenterservice/models/vo/ActivityPriceParamVO.java
+++ mktcenter-service/src/main/java/com/bizvane/mktcenterservice/models/vo/ActivityPriceParamVO.java
@@ -1,8 +1,14 @@
 package com.bizvane.mktcenterservice.models.vo;
 
+import lombok.Data;
+
 /**
  * @Author: lijunwei
  * @Time: 2018/12/19 17:28
  */
+@Data
 public class ActivityPriceParamVO {
+    private String activityCode;
+    private String activityName;
+    private Integer activityStatus;
 }
