7d6af0568af64ba1be9236c789c15ad021f887f0
chen.li <lich1n@163.com>
2018-12-19 15:53:17 +0800

    增加是否中奖字段

--- mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/mappers/MktActivityPrizeRecordPOMapper.java
+++ mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/mappers/MktActivityPrizeRecordPOMapper.java
@@ -6,80 +6,72 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 public interface MktActivityPrizeRecordPOMapper {
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2018-12-18 15:22:31
-     */
-    long countByExample(MktActivityPrizeRecordPOExample example);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2018-12-18 15:22:31
-     */
-    int deleteByExample(MktActivityPrizeRecordPOExample example);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2018-12-19 15:52:22
+	 */
+	long countByExample(MktActivityPrizeRecordPOExample example);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2018-12-18 15:22:31
-     */
-    int deleteByPrimaryKey(Long mktActivityPrizeRecordId);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2018-12-19 15:52:22
+	 */
+	int deleteByExample(MktActivityPrizeRecordPOExample example);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2018-12-18 15:22:31
-     */
-    int insert(MktActivityPrizeRecordPO record);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2018-12-19 15:52:22
+	 */
+	int deleteByPrimaryKey(Long mktActivityPrizeRecordId);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2018-12-18 15:22:31
-     */
-    int insertSelective(MktActivityPrizeRecordPO record);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2018-12-19 15:52:22
+	 */
+	int insert(MktActivityPrizeRecordPO record);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2018-12-18 15:22:31
-     */
-    List<MktActivityPrizeRecordPO> selectByExample(MktActivityPrizeRecordPOExample example);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2018-12-19 15:52:22
+	 */
+	int insertSelective(MktActivityPrizeRecordPO record);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2018-12-18 15:22:31
-     */
-    MktActivityPrizeRecordPO selectByPrimaryKey(Long mktActivityPrizeRecordId);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2018-12-19 15:52:22
+	 */
+	List<MktActivityPrizeRecordPO> selectByExample(MktActivityPrizeRecordPOExample example);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2018-12-18 15:22:31
-     */
-    int updateByExampleSelective(@Param("record") MktActivityPrizeRecordPO record, @Param("example") MktActivityPrizeRecordPOExample example);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2018-12-19 15:52:22
+	 */
+	MktActivityPrizeRecordPO selectByPrimaryKey(Long mktActivityPrizeRecordId);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2018-12-18 15:22:31
-     */
-    int updateByExample(@Param("record") MktActivityPrizeRecordPO record, @Param("example") MktActivityPrizeRecordPOExample example);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2018-12-19 15:52:22
+	 */
+	int updateByExampleSelective(@Param("record") MktActivityPrizeRecordPO record,
+			@Param("example") MktActivityPrizeRecordPOExample example);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2018-12-18 15:22:31
-     */
-    int updateByPrimaryKeySelective(MktActivityPrizeRecordPO record);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2018-12-19 15:52:22
+	 */
+	int updateByExample(@Param("record") MktActivityPrizeRecordPO record,
+			@Param("example") MktActivityPrizeRecordPOExample example);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2018-12-18 15:22:31
-     */
-    int updateByPrimaryKey(MktActivityPrizeRecordPO record);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2018-12-19 15:52:22
+	 */
+	int updateByPrimaryKeySelective(MktActivityPrizeRecordPO record);
+
+	/**
+	 * 只读. 
+	 * @mbg.generated  2018-12-19 15:52:22
+	 */
+	int updateByPrimaryKey(MktActivityPrizeRecordPO record);
 }
