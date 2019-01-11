fac2ecd4b79c5b857095de1ea4ef4ce0ecc0ec3c
lijunwei <lijunwei@bizvane.cn>
2019-01-11 14:57:56 +0800

    修改PO

--- mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/mappers/MktActivityRedPacketSumPOMapper.java
+++ mktcenter-serviceimpl/src/main/java/com/bizvane/mktcenterserviceimpl/mappers/MktActivityRedPacketSumPOMapper.java
@@ -6,80 +6,72 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 public interface MktActivityRedPacketSumPOMapper {
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2019-01-11 11:03:19
-     */
-    long countByExample(MktActivityRedPacketSumPOExample example);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2019-01-11 11:03:19
-     */
-    int deleteByExample(MktActivityRedPacketSumPOExample example);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2019-01-11 14:54:58
+	 */
+	long countByExample(MktActivityRedPacketSumPOExample example);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2019-01-11 11:03:19
-     */
-    int deleteByPrimaryKey(Long mktActivityRedPacketSumId);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2019-01-11 14:54:58
+	 */
+	int deleteByExample(MktActivityRedPacketSumPOExample example);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2019-01-11 11:03:19
-     */
-    int insert(MktActivityRedPacketSumPO record);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2019-01-11 14:54:58
+	 */
+	int deleteByPrimaryKey(Long mktActivityRedPacketSumId);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2019-01-11 11:03:19
-     */
-    int insertSelective(MktActivityRedPacketSumPO record);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2019-01-11 14:54:58
+	 */
+	int insert(MktActivityRedPacketSumPO record);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2019-01-11 11:03:19
-     */
-    List<MktActivityRedPacketSumPO> selectByExample(MktActivityRedPacketSumPOExample example);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2019-01-11 14:54:58
+	 */
+	int insertSelective(MktActivityRedPacketSumPO record);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2019-01-11 11:03:19
-     */
-    MktActivityRedPacketSumPO selectByPrimaryKey(Long mktActivityRedPacketSumId);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2019-01-11 14:54:58
+	 */
+	List<MktActivityRedPacketSumPO> selectByExample(MktActivityRedPacketSumPOExample example);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2019-01-11 11:03:19
-     */
-    int updateByExampleSelective(@Param("record") MktActivityRedPacketSumPO record, @Param("example") MktActivityRedPacketSumPOExample example);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2019-01-11 14:54:58
+	 */
+	MktActivityRedPacketSumPO selectByPrimaryKey(Long mktActivityRedPacketSumId);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2019-01-11 11:03:19
-     */
-    int updateByExample(@Param("record") MktActivityRedPacketSumPO record, @Param("example") MktActivityRedPacketSumPOExample example);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2019-01-11 14:54:58
+	 */
+	int updateByExampleSelective(@Param("record") MktActivityRedPacketSumPO record,
+			@Param("example") MktActivityRedPacketSumPOExample example);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2019-01-11 11:03:19
-     */
-    int updateByPrimaryKeySelective(MktActivityRedPacketSumPO record);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2019-01-11 14:54:58
+	 */
+	int updateByExample(@Param("record") MktActivityRedPacketSumPO record,
+			@Param("example") MktActivityRedPacketSumPOExample example);
 
-    /**
-     * 只读. 
-     *
-     * @mbg.generated 2019-01-11 11:03:19
-     */
-    int updateByPrimaryKey(MktActivityRedPacketSumPO record);
+	/**
+	 * 只读. 
+	 * @mbg.generated  2019-01-11 14:54:58
+	 */
+	int updateByPrimaryKeySelective(MktActivityRedPacketSumPO record);
+
+	/**
+	 * 只读. 
+	 * @mbg.generated  2019-01-11 14:54:58
+	 */
+	int updateByPrimaryKey(MktActivityRedPacketSumPO record);
 }
