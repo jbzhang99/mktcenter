c8f5262f2cee17d8c1b68a8a3ac859c270a3d852
lijunwei <lijunwei@bizvane.cn>
2019-01-21 16:05:11 +0800

    ok005

--- mktcenter-service/src/main/java/com/bizvane/mktcenterservice/models/vo/DefinedVO.java
+++ mktcenter-service/src/main/java/com/bizvane/mktcenterservice/models/vo/DefinedVO.java
@@ -1,9 +1,12 @@
 package com.bizvane.mktcenterservice.models.vo;
 
+import lombok.Data;
+
 /**
  * @Author: lijunwei
  * @Time: 2019/1/21 15:59
  */
+@Data
 public class DefinedVO {
     private String wxNickname;
     private String wxHeadUrl;
