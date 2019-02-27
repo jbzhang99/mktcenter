64b1fa4ca3798d6f464fca2af56672fc51944f3b
Liufq <414804772@qq.com>
2019-02-27 15:29:24 +0800

    update 添加砸活动统计RPC

---
+++ mktcenter-service/src/main/java/com/bizvane/mktcenterservice/rpc/ActivityGoldenStatisticsServiceRpc.java
@@ -0,0 +1,22 @@
+package com.bizvane.mktcenterservice.rpc;
+
+import com.bizvane.mktcenterservice.models.bo.ActivityGoldenStatisticsBo;
+import com.bizvane.utils.responseinfo.ResponseData;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author liufq
+ */
+@FeignClient(value = "${feign.client.mktcenter.name}",path = "${feign.client.mktcenter.path}/goldenStatisticsRpc")
+public interface ActivityGoldenStatisticsServiceRpc {
+    /**
+     * 砸金蛋活动统计
+     *
+     * @param bo
+     * @return
+     * */
+    @RequestMapping("/goldenStatisticsData")
+    ResponseData goldenStatisticsData(@RequestBody ActivityGoldenStatisticsBo bo);
+}
