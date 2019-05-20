56d7b8a641ee4f7ee66ceed98252afbe3c704d90
lich1n@aliyun.com <tangguaner2019>
2019-05-20 13:17:32 +0800

    增加注释

--- mktcenter-facade/src/main/java/com/bizvane/mktcenterfacade/models/bo/CouponIntegralExchangeBO.java
+++ mktcenter-facade/src/main/java/com/bizvane/mktcenterfacade/models/bo/CouponIntegralExchangeBO.java
@@ -2,13 +2,16 @@ package com.bizvane.mktcenterfacade.models.bo;
 
 import com.bizvane.mktcenterfacade.models.vo.MktCouponIntegralExchangeVO;
 import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.ApiModelProperty;
 
 /**
  * @Author: lijunwei
  * @Time: 2018/11/28 14:34
  */
 public class CouponIntegralExchangeBO {
+    @ApiModelProperty(name = "pageInfoMktCouponIntegralExchangeVO", value = "出参数据")
     private PageInfo<MktCouponIntegralExchangeVO> pageInfo;
+    @ApiModelProperty(name = "countIntegral", value = "会员当前积分")
     private Integer countIntegral;
 
     public PageInfo<MktCouponIntegralExchangeVO> getPageInfo() {
--- mktcenter-facade/src/main/java/com/bizvane/mktcenterfacade/models/vo/MktCouponIntegralExchangeVO.java
+++ mktcenter-facade/src/main/java/com/bizvane/mktcenterfacade/models/vo/MktCouponIntegralExchangeVO.java
@@ -4,6 +4,7 @@ import com.bizvane.centerstageservice.models.po.SysStorePo;
 import com.bizvane.couponfacade.models.po.CouponDefinitionPO;
 import com.bizvane.couponfacade.models.vo.CouponDetailResponseVO;
 import com.bizvane.mktcenterfacade.models.po.MktCouponIntegralExchangePO;
+import io.swagger.annotations.ApiModelProperty;
 
 import java.util.List;
 
@@ -11,17 +12,24 @@ import java.util.List;
  * Created by agan on 2018/11/22.
  */
 public class MktCouponIntegralExchangeVO extends MktCouponIntegralExchangePO {
+    @ApiModelProperty(name = "startTime", value = "券有效期的开始时间")
     private String startTime;
+    @ApiModelProperty(name = "endTime", value = "券有效期的结束时间")
     private  String endTime;
-
+    @ApiModelProperty(name = "exchangeIds", value = "兑换券id集合")
     private List<Long> exchangeIds;//id集合
+    @ApiModelProperty(name = "restCount", value = "剩余库存")
     private Integer restCount;//剩余库存
     //门店实体类
+    @ApiModelProperty(name = "sysStorePos", value = "门店信息")
     private List<SysStorePo> sysStorePos;
     //券剩余数量
+    @ApiModelProperty(name = "residueCouponNumber", value = "券剩余数量")
     private Integer residueCouponNumber;
+    @ApiModelProperty(name = "couponDefinitionPO", value = "券定义实体")
     private CouponDefinitionPO couponDefinitionPO;
     //券
+    @ApiModelProperty(name = "couponDetailResponseVO", value = "券详情实体")
     private CouponDetailResponseVO couponDetailResponseVO;
 
     public List<Long> getExchangeIds() {
