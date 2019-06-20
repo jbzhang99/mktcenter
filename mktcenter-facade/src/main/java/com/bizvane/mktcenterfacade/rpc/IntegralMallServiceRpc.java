f8ba84218b4b7c20326f830f0f2ed819665aa996
董争光 <welcometomasm@foxmail.com>
2019-06-20 11:36:13 +0800

    积分商城bug

--- mktcenter-facade/src/main/java/com/bizvane/mktcenterfacade/rpc/IntegralMallServiceRpc.java
+++ mktcenter-facade/src/main/java/com/bizvane/mktcenterfacade/rpc/IntegralMallServiceRpc.java
@@ -1,19 +1,18 @@
 package com.bizvane.mktcenterfacade.rpc;
 
-import com.bizvane.centercontrolservice.models.bo.AppletFunctionBO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.bizvane.mktcenterfacade.models.bo.CouponIntegralExchangeBO;
 import com.bizvane.mktcenterfacade.models.po.MktConvertCouponRecordPO;
 import com.bizvane.mktcenterfacade.models.vo.CouponIntegralExchangeVO;
 import com.bizvane.mktcenterfacade.models.vo.CouponRecordVO;
 import com.bizvane.utils.responseinfo.ResponseData;
 import com.github.pagehelper.PageInfo;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * Created by Geng on 2018/8/9.
+ * 堂倌
  *  积分兑换券
  */
 
@@ -22,14 +21,17 @@ public interface IntegralMallServiceRpc {
 
     //查询对换列表
     @RequestMapping("/getConvernCouponLists")
-    public ResponseData<CouponIntegralExchangeBO> getConvernCouponLists(@RequestBody CouponRecordVO vo);
-   //券详情和单价
+    ResponseData<CouponIntegralExchangeBO> getConvernCouponLists(@RequestBody CouponRecordVO vo);
+    
+    //券详情和单价
     @RequestMapping("/getCouponAndPrice")
-    public ResponseData<CouponIntegralExchangeVO> getCouponAndPrice(@RequestBody CouponRecordVO vo);
+    ResponseData<CouponIntegralExchangeVO> getCouponAndPrice(@RequestBody CouponRecordVO vo);
+    
     //兑换
     @RequestMapping("/doConvernCoupon")
-    public ResponseData<Integer> doConvernCoupon(@RequestBody CouponRecordVO vo);
+    ResponseData<Integer> doConvernCoupon(@RequestBody CouponRecordVO vo);
+    
     //查询已兑换
     @RequestMapping("/getConvernConpouByMember")
-    public ResponseData<PageInfo<MktConvertCouponRecordPO>> getConvernConpouByMember(@RequestBody CouponRecordVO vo);
+    ResponseData<PageInfo<MktConvertCouponRecordPO>> getConvernConpouByMember(@RequestBody CouponRecordVO vo);
 }
--- mktcenter-service/src/main/java/com/bizvane/mktcenterservice/controllers/rpc/IntegralMallRpcController.java
+++ mktcenter-service/src/main/java/com/bizvane/mktcenterservice/controllers/rpc/IntegralMallRpcController.java
@@ -1,19 +1,18 @@
 package com.bizvane.mktcenterservice.controllers.rpc;
 
-import com.bizvane.mktcenterfacade.interfaces.ConvertCouponService;
-import com.bizvane.mktcenterfacade.models.bo.CouponIntegralExchangeBO;
-import com.bizvane.mktcenterfacade.models.po.MktActivityRecordPO;
-import com.bizvane.mktcenterfacade.models.po.MktConvertCouponRecordPO;
-import com.bizvane.mktcenterfacade.models.vo.CouponRecordVO;
-import com.bizvane.mktcenterfacade.models.vo.MktActivityRecordVO;
-import com.bizvane.utils.responseinfo.ResponseData;
-import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.bizvane.mktcenterfacade.interfaces.ConvertCouponService;
+import com.bizvane.mktcenterfacade.models.bo.CouponIntegralExchangeBO;
+import com.bizvane.mktcenterfacade.models.po.MktConvertCouponRecordPO;
+import com.bizvane.mktcenterfacade.models.vo.CouponIntegralExchangeVO;
+import com.bizvane.mktcenterfacade.models.vo.CouponRecordVO;
+import com.bizvane.mktcenterfacade.rpc.IntegralMallServiceRpc;
+import com.bizvane.utils.responseinfo.ResponseData;
+import com.github.pagehelper.PageInfo;
 
 /**
  * @author chen.li
@@ -22,26 +21,32 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("integralMallServiceRpc")
-public class IntegralMallRpcController {
+public class IntegralMallRpcController implements IntegralMallServiceRpc {
 
     @Autowired
     private ConvertCouponService convertCouponService;
 
     //积分兑换券
     //查询对换列表
-    @RequestMapping("/getConvernCouponLists")
+    @Override
     public ResponseData<CouponIntegralExchangeBO> getConvernCouponLists(@RequestBody CouponRecordVO vo){
         return convertCouponService.getConvernCouponLists(vo);
     }
 
     //兑换
-    @RequestMapping("/doConvernCoupon")
+    @Override
     public ResponseData<Integer> doConvernCoupon(@RequestBody CouponRecordVO vo){
         return convertCouponService.doConvernCoupon(vo);
     }
     //查询已兑换
-    @RequestMapping("/getConvernConpouByMember")
+    @Override
     public ResponseData<PageInfo<MktConvertCouponRecordPO>> getConvernConpouByMember(@RequestBody CouponRecordVO vo){
         return convertCouponService.getConvernConpouByMember(vo);
+    }
+
+    @Override
+    public ResponseData<CouponIntegralExchangeVO> getCouponAndPrice(@RequestBody CouponRecordVO vo) {
+      // TODO Auto-generated method stub
+      return convertCouponService.getCouponAndPrice(vo);
     }
 }
